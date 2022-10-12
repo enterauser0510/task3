9ed2b1b7befac78d2c2b3c784144c0b726fe70e1
enterauser0510 <115065002+enterauser0510@users.noreply.github.com>
2022-10-12 14:52:00 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,39 @@
-basic.showNumber(1)
-basic.showNumber(2)
-basic.showNumber(3)
-basic.showNumber(4)
-basic.showNumber(5)
-basic.showNumber(6)
-basic.forever(function () {
-	
+let count = 0
+input.onButtonPressed(Button.A, function () {
+    count = 9
+    while (count >= 0) {
+        basic.showNumber(count)
+        count += -1
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        # . . . #
+        . . # . .
+        # . . . #
+        . . # . .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    count = 0
 })
